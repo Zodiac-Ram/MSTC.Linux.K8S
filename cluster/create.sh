1de3bc313c9b82ce1df2cfd5798ff39636a9a8ed
George Heeres <gheeres@gmail.com>
2023-12-05 16:56:36 -0600

    Add kubernetes config copy/setup

--- cluster/create.sh
+++ cluster/create.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 sudo kubeadm config images pull
-IPADDR="192.168.254.128"
+IPADDR="10.31.1.101"
 POD_CIDR="172.31.0.0/16"
 sudo kubeadm init --apiserver-advertise-address=$IPADDR --pod-network-cidr=$POD_CIDR
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl get pods -n kube-system
